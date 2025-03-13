b91db62e5d09e875ee680eb51a212d5481f79fb6
rishireddy26 <162895030+rishireddy26@users.noreply.github.com>
2025-03-13 12:08:22 +0530

    hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,10 @@
+//File: hello.cpp
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Hello, Jenkins! Task 2 is running successfully!" << endl;
-    return 0;
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  return 0;
 }
